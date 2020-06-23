e84398b4a64aaf25d32e1014f39d9df653e3b115
Someone Somewhere <sackboydj167@gmail.com>
2020-06-23 21:34:17 +0100

    Added ObjectIDManager

---
+++ Source/Core/ObjectIDManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MultiplayerMod.Core
+{
+    public static class ObjectIDManager
+    {
+        private static Dictionary<ushort, GameObject> objects = new Dictionary<ushort, GameObject>();
+
+        public static void Reset()
+        {
+            objects.Clear();
+        }
+
+        public static GameObject GetObject(ushort id)
+        {
+            return objects[id];
+        }
+
+        public static void AddObject(ushort id, GameObject obj)
+        {
+            objects.Add(id, obj);
+        }
+    }
+}
