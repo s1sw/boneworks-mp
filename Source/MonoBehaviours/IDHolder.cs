bf3d9a7d8d2d4e07949efd18830fe671222d4494
Someone Somewhere <sackboydj167@gmail.com>
2020-06-23 21:21:39 +0100

    Object ID holder component

---
+++ Source/MonoBehaviours/IDHolder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace MultiplayerMod.MonoBehaviours
+{
+    public class IDHolder : MonoBehaviour
+    {
+        public IDHolder(IntPtr ptr) : base(ptr) { }
+
+        public short ID;
+    }
+}
